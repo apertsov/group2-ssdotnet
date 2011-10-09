e1790b5e077f1f6715c4d46becc31bc146d3dab8
pavlo.sobchuk@gmail.com <pavlo.sobchuk@gmail.com@dc9dbde5-9d39-9759-6e65-f2745b6244c2>
2011-10-09 20:03:19 +0000

    Увага! Обновив БД! Перезалийте собі бд на сервера. Там добавлено нове поле до таблиці Employee. Готово додавання + відображення пацієнтів. (примітивно, але завтра можна буде показати)

---
+++ DiagnosticCenter/DiagnosticCenter/Controllers/PatientsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DiagnosticCenter.Models;
+
+namespace DiagnosticCenter.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private DiagnosticsDBModelContainer _patients = new DiagnosticsDBModelContainer();
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Patient new_Patient)
+        {
+            _patients.AddToPatients(new_Patient);
+            _patients.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Index()
+        {
+            return View(_patients.Patients.ToList());
+        }
+    }
+}
