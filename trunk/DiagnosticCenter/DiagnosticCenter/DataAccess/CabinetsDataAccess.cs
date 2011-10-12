bc2ff7a826bcb09bdcc2a27899411c3aa4ca4cd1
grigorashik@gmail.com <grigorashik@gmail.com@dc9dbde5-9d39-9759-6e65-f2745b6244c2>
2011-10-12 10:28:58 +0000

    Додав розбитя на стрінки для кабінетів

---
+++ trunk/DiagnosticCenter/DiagnosticCenter/DataAccess/CabinetsDataAccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DiagnosticCenter.Models;
+using System.Data;
+
+namespace DiagnosticCenter
+{
+    public class CabinetsDataAccess
+    {
+
+        public static PagedList<Cabinet> GetPagedCabinets(int skip, int take)
+        {
+            using (var context = new DiagnosticsDBModelContainer())
+            {
+
+                //refactor consideration...make this a pre-compiled query
+                var query = context.Cabinets
+                            .Where(c => c.ID_Cabinet > 0)
+                            .OrderBy(c => c.Number);
+
+                //getting this count every time in case others are adding or deleting customers
+                //decide for yourself in your app
+                var customerCount = query.Count();
+
+                var customers = query.Skip(skip).Take(take).ToList();
+
+                return new PagedList<Cabinet>
+                {
+                    Entities = customers,
+//                                  !!!!!
+                    HasNext = (skip + 10 < customerCount),
+                    HasPrevious = (skip > 0)
+                };
+            }
+        }
+
+        public static Cabinet GetCabinetById(int id)
+        {
+            using (var context = new DiagnosticsDBModelContainer())
+            {
+                //refactor consideration...make this a pre-compiled query
+                var query = context.Cabinets
+                            .Where (c => c.ID_Cabinet == id);
+
+                return query.SingleOrDefault(); //non-demo app should consider exception handling
+            }
+        }
+    }
+
+    public class PagedList<T>
+    {
+        public bool HasNext { get; set; }
+        public bool HasPrevious { get; set; }
+        public List<T> Entities { get; set; }
+    }
+}
