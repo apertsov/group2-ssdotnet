932311b64269c5a657e5b711abd6108c6fe8f008
LysakBogdan@gmail.com <LysakBogdan@gmail.com@dc9dbde5-9d39-9759-6e65-f2745b6244c2>
2011-10-15 19:22:10 +0000

    довавив можливість редагування для відділень

--- DiagnosticCenter/DiagnosticCenter/Models/Departments.cs
+++ DiagnosticCenter/DiagnosticCenter/Models/Departments.cs
@@ -14,7 +14,7 @@ namespace DiagnosticCenter.Models
         }
         public List<Department> getList()
         {   
-            return (from d in DB.Departments select d).ToList();
+            return (from d in DB.Departments orderby d.Name select d).ToList();
         }
         public Department getByID(int id)
         {
@@ -25,6 +25,16 @@ namespace DiagnosticCenter.Models
             List<Department> list = (from a in DB.Departments select a).ToList();
             int max = list.Count == 0 ? 1 : list.Max(x => x.ID_Dept);
             d.ID_Dept = max + 1;
+            DB.Departments.AddObject(d);
+            DB.SaveChanges();
+            return d;
+        }
+        public Department update(int id, Department d)
+        {
+            var X = from x in DB.Departments where x.ID_Dept == id select x;
+            if (X.First() == null) return null;
+            d.ID_Dept = X.First().ID_Dept;
+            DB.Departments.DeleteObject(X.First());
             DB.Departments.AddObject(d);
             DB.SaveChanges();
             return d;
