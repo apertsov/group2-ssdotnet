ddb65a9a3b76bc191f20e46f9da0ebab583c3cbd
pavlo.sobchuk@gmail.com <pavlo.sobchuk@gmail.com@dc9dbde5-9d39-9759-6e65-f2745b6244c2>
2011-10-29 18:51:06 +0000

    Виписування і роздруковування направлення з неймовірно-динамічно-інтерактивними кнопками і дроплістами

--- DiagnosticCenter/DiagnosticCenter/Models/ReferralVM.cs
+++ DiagnosticCenter/DiagnosticCenter/Models/ReferralVM.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace DiagnosticCenter.Models
 {
     public class ReferralVM
     {
         public string patient { get; set; }
+        public int idp { get; set; }
         public List<SelectListItem> department { get; set; }
         public List<SelectListItem> cabinet { get; set; }
         public List<SelectListItem> employee { get; set; }
+
+        [Required(ErrorMessage = "Обов'язкове поле")]
+        [DataType(DataType.DateTime,ErrorMessage="Невірний формат")]
         public DateTime visitDate { get; set; }
+        
         public DateTime todayDate { get; set; }
 
         public void SetModel(int pat)
@@ -26,6 +33,7 @@ namespace DiagnosticCenter.Models
             this.todayDate = DateTime.Now;
             this.cabinet = GetCabinet(dept[0].Name);
             this.employee = GetEmployee(this.cabinet.First().Text);
+            this.idp = pat;
             
         }
         public List<SelectListItem> GetCabinet(string department)
@@ -34,15 +42,25 @@ namespace DiagnosticCenter.Models
             List<Cabinet> cab = context.Cabinets.Include("Department").ToList();
             IEnumerable<SelectListItem> _cab = cab.Where(e => e.Department.Name == department)
                                                   .Select(e => new SelectListItem { Value = e.Number.ToString(), Text = e.Number.ToString() });
-            return _cab.ToList();
+            List<SelectListItem> c = _cab.ToList();
+            SelectListItem i = new SelectListItem();
+            i.Text = "--Кабінет--";
+            i.Value = "0";
+            c.Insert(0, i);
+            return c;
         }
         public List<SelectListItem> GetEmployee(string number)
         {
             DiagnosticsDBModelContainer context = new DiagnosticsDBModelContainer();
             List<Employee> empl = context.Employees.Include("Cabinet").ToList();
             IEnumerable<SelectListItem> _empl = empl.Where(e => e.Cabinet.Number.ToString() == number)
-                                                    .Select(e => new SelectListItem { Value = e.FirstName + " " + e.Surname, Text = e.FirstName + " " + e.Surname });
-            return _empl.ToList();
+                                                    .Select(e => new SelectListItem { Value = e.ID_Employee.ToString(), Text = e.FirstName + " " + e.Surname });
+            List<SelectListItem> c = _empl.ToList();
+            SelectListItem i = new SelectListItem();
+            i.Text = "--Лікар--";
+            i.Value = "0";
+            c.Insert(0, i);
+            return  c;
         }
     }
 }
