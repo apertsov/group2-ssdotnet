71c0508b035b5db6b717e042e5780a5a47551804
LysakBogdan@gmail.com <LysakBogdan@gmail.com@dc9dbde5-9d39-9759-6e65-f2745b6244c2>
2011-12-11 16:19:56 +0000

    Авторизація в чаті.

--- DiagnosticCenter/ChatCore/ChatCore/Service1.cs
+++ DiagnosticCenter/ChatCore/ChatCore/Service1.cs
@@ -6,6 +6,8 @@ using System.ServiceModel;
 using System.Text;
 using System.IO;
 
+using System.Data.SqlClient;
+
 class ChatUser
 {
     public string Name;
@@ -72,12 +74,62 @@ namespace ChatCore
                 accounts.Add( new Account(){ Login = fs.ReadLine(), Password = fs.ReadLine()});
             fs.Close();
         }
+/***************************************/
+        public static bool ExistsEmploye(string userName)
+        {
+            using (SqlConnection connection = new SqlConnection("server=.\\SQLEXPRESS;Trusted_connection=yes;database=DiagnosticsDB;connection timeout=30"))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT Username FROM Employees;";
+                try
+                {
+                    connection.Open();
 
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                        if (userName == reader[0].ToString())
+                            return true;
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return false;
+        }
+        public static bool Authenticate(string userName, string password)
+        {
+            using (SqlConnection connection = new SqlConnection("server=.\\SQLEXPRESS;Trusted_connection=yes;database=DiagnosticsDB;connection timeout=30"))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT Username,Password FROM Users;";
+                try
+                {
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                        if (userName == reader[0].ToString() && password == reader[1].ToString())
+                            return true;
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return false;
+        }
+/***************************************/
         public List<string> Join(string name,string password)
         {
-            if (password != null && !accounts.Exists(a => a.Login == name && a.Password == password))
+            if (password != null && !Authenticate( name, password ) )
                         return null;
-            if (password == null && accounts.Exists(a => a.Login == name)) return null;
+
+            if (password == null && ExistsEmploye(name)) return null;
 
             IChatCallback callback = OperationContext.Current.GetCallbackChannel<IChatCallback>();
 
@@ -97,6 +149,11 @@ namespace ChatCore
             Console.WriteLine(callback);
 
             return res;
+        }
+
+        private bool ExistsEmpoye(string name)
+        {
+            throw new NotImplementedException();
         }
 
         public void Send(string msg)
